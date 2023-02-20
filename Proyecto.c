091252ed687813ad456df5b346621bf59f5e2275
ManuelRG18 <ManuelRGlod18@gmail.com>
2023-02-20 14:02:39 -0400

    menu principal

--- Proyecto.c
+++ Proyecto.c
@@ -18,56 +18,62 @@ typedef struct usuarios
 
 
 
-int main (){
+int main (void){
+system ("cls");
+int opc;
+char temp [10];
+fgets(temp,10,stdin);
+opc = atoi (temp);
+do
+{
+   printf("bienvenido al sistema");
+ printf("\n\t---------------------------------------------");
+        printf("\n\n\t\t1.-Crear usuario");
+        printf("\n\t\t2.-login admin");
+        printf("\n\t\t3.-login cliente");
+		printf("\n\t\t4.-Salir");
+        printf("\n\t---------------------------------------------");
+        switch (opc)
+        {
+        case 1:{
 
-void crearusu();
+
+         break;
+        } 
+        case 2:{
+
+         break;   
+        }
+        case 3:{
 
 
+         break;
+        }
 
+        
+        default:
+            
+            printf("opcion incorrecta \n");
 
+            break;
 
+        }
 
+} while (opc != 4);
 
+return 0;
 }
 
+
+
+
+
+
+
+
+
 void crearusu(){
- int i;
- usuarios user [2];
-  char userName[20], password[20]; // Variables temporales para guardar los datos
-    FILE *userData; // Archivo donde se guardarán los datos de los usuarios
-   // Solicita los datos a dos usuarios
-    userData = fopen("userdata.txt","a");
-    for (i = 0; i < 2; i++)
-    {
-        printf("Usuario %d\n", i + 1);
-        printf("Nombre de usuario: ");
-        scanf("%s", userName);
-        printf("Contraseña: ");
-        scanf("%s", password);
-        // Guarda los datos de los usuarios en la estructura
-        strcpy(user[i].usu, userName);
-        strcpy(user[i].pass, password);
-    }
  
-    // Abre el archivo donde se guardarán los datos y lo escribe
-    userData = fopen("userData.txt", "w");
-    for (i = 0; i < 2; i++)
-    {
-        fprintf(userData, "%s %s\n", user[i].usu, user[i].pass);
-    }
-    fclose(userData);
- 
-    // Lee el archivo y valida los datos de los usuarios
-    userData = fopen("userData.txt", "r");
-    for (i = 0; i < 2; i++)
-    {
-        fscanf(userData, "%s %s\n", userName, password);
-        if (strcmp(userName, user[i].usu) == 0 && strcmp(password, user[i].pass) == 0)
-            printf("Los datos del usuario %d son correctos\n", i + 1);
-        else
-            printf("Los datos del usuario %d son incorrectos\n", i + 1);
-    }
-    fclose(userData);
     
 }
 
